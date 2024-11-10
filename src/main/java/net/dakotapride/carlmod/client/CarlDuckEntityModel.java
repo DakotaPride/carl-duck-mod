ccac046201dd64e1a02f3d28887b49e6b47578c3
Dakota <rjtube13@gmail.com>
2024-11-10 11:46:17 -0600

    1.8.3 - Carl: Into the Carlverse

--- src/main/java/net/dakotapride/carlmod/client/CarlDuckEntityModel.java
+++ src/main/java/net/dakotapride/carlmod/client/CarlDuckEntityModel.java
@@ -16,6 +16,12 @@ public class CarlDuckEntityModel extends GeoModel<CarlDuckEntity> {
         if ("god".equalsIgnoreCase(object.getName().getString()) || "angel".equalsIgnoreCase(object.getName().getString())
                 || "biblically_accurate".equalsIgnoreCase(object.getName().toString()) || "biblically_accurate_carl".equalsIgnoreCase(object.getName().getString())) {
             return new ResourceLocation(CarlMod.MODID, "geo/biblically_accurate_carl.geo.json");
+        } else if ("lamb".equalsIgnoreCase(object.getName().getString()) || "cult_leader".equalsIgnoreCase(object.getName().getString())) {
+            return new ResourceLocation(CarlMod.MODID, "geo/carl_cult_leader.geo.json");
+        } else if ("braincell".equalsIgnoreCase(object.getName().getString()) || "braincells".equalsIgnoreCase(object.getName().getString())) {
+            return new ResourceLocation(CarlMod.MODID, "geo/braincells.geo.json");
+        } else if ("hooty".equalsIgnoreCase(object.getName().getString()) || "hootsifer".equalsIgnoreCase(object.getName().getString())) {
+            return new ResourceLocation(CarlMod.MODID, "geo/hoot.geo.json");
         }
         else return new ResourceLocation(CarlMod.MODID, "geo/carl.geo.json");
     }
@@ -30,6 +36,10 @@ public class CarlDuckEntityModel extends GeoModel<CarlDuckEntity> {
         if ("god".equalsIgnoreCase(animatable.getName().getString()) || "angel".equalsIgnoreCase(animatable.getName().getString())
                 || "biblically_accurate".equalsIgnoreCase(animatable.getName().getString()) || "biblically_accurate_carl".equalsIgnoreCase(animatable.getName().getString())) {
             return new ResourceLocation(CarlMod.MODID, "animations/biblically_accurate_carl.animation.json");
+        } else if ("lamb".equalsIgnoreCase(animatable.getName().getString()) || "cult_leader".equalsIgnoreCase(animatable.getName().getString())) {
+            return new ResourceLocation(CarlMod.MODID, "animations/carl_cult_leader.animation.json");
+        } else if ("hooty".equalsIgnoreCase(animatable.getName().getString()) || "hootsifer".equalsIgnoreCase(animatable.getName().getString())) {
+            return new ResourceLocation(CarlMod.MODID, "animations/hoot.animation.json");
         }
         else return new ResourceLocation(CarlMod.MODID, "animations/carl.animation.json");
     }
--- src/main/java/net/dakotapride/carlmod/client/CarlDuckEntityRenderer.java
+++ src/main/java/net/dakotapride/carlmod/client/CarlDuckEntityRenderer.java
@@ -2,9 +2,12 @@ package net.dakotapride.carlmod.client;
 
 import net.dakotapride.carlmod.CarlDuckEntity;
 import net.dakotapride.carlmod.CarlMod;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class CarlDuckEntityRenderer extends GeoEntityRenderer<CarlDuckEntity> {
@@ -45,6 +48,40 @@ public class CarlDuckEntityRenderer extends GeoEntityRenderer<CarlDuckEntity> {
                 || "biblically_accurate".equalsIgnoreCase(instance.getName().getString()) || "biblically_accurate_carl".equalsIgnoreCase(instance.getName().getString())) ?
                 new ResourceLocation(CarlMod.MODID, "textures/entity/biblically_accurate_carl.png")
 
+                : ("lamb".equalsIgnoreCase(instance.getName().getString()) || "cult_leader".equalsIgnoreCase(instance.getName().getString())) ?
+                new ResourceLocation(CarlMod.MODID, "textures/entity/lamb.png")
+
+                : ("steel_spacesuit".equalsIgnoreCase(instance.getName().getString()) || "steel_space_suit".equalsIgnoreCase(instance.getName().getString())) ?
+                new ResourceLocation(CarlMod.MODID, "textures/entity/steel_spacesuit.png")
+
+                : ("calorite_spacesuit".equalsIgnoreCase(instance.getName().getString()) || "calorite_space_suit".equalsIgnoreCase(instance.getName().getString())) ?
+                new ResourceLocation(CarlMod.MODID, "textures/entity/calorite_spacesuit.png")
+
+                : ("netherite_spacesuit".equalsIgnoreCase(instance.getName().getString()) || "netherite_space_suit".equalsIgnoreCase(instance.getName().getString())) ?
+                new ResourceLocation(CarlMod.MODID, "textures/entity/netherite_spacesuit.png")
+
+                : ("braincell".equalsIgnoreCase(instance.getName().getString()) || "braincells".equalsIgnoreCase(instance.getName().getString())) ?
+                new ResourceLocation(CarlMod.MODID, "textures/entity/braincells.png")
+
+                : ("copycat".equalsIgnoreCase(instance.getName().getString()) || "copycats".equalsIgnoreCase(instance.getName().getString())) ?
+                new ResourceLocation(CarlMod.MODID, "textures/entity/copycats.png")
+
+//                : ("trans_rights".equalsIgnoreCase(instance.getName().getString()) || "transgender".equalsIgnoreCase(instance.getName().getString())) ?
+//                new ResourceLocation(CarlMod.MODID, "textures/entity/trans_rights.png")
+
+                : ("helmetless".equalsIgnoreCase(instance.getName().getString())) ?
+                new ResourceLocation(CarlMod.MODID, "textures/entity/helmetless_carl.png")
+
+                : ("hooty".equalsIgnoreCase(instance.getName().getString()) || "hootsifer".equalsIgnoreCase(instance.getName().getString())) ?
+                new ResourceLocation(CarlMod.MODID, "textures/entity/hoot.png")
+
                 : new ResourceLocation(CarlMod.MODID, "textures/entity/carl.png");
+    }
+
+    @Override
+    public RenderType getRenderType(CarlDuckEntity animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
+        if ("steel_spacesuit".equalsIgnoreCase(animatable.getName().getString()) || "calorite_spacesuit".equalsIgnoreCase(animatable.getName().getString()) || "netherite_spacesuit".equalsIgnoreCase(animatable.getName().getString())) {
+            return RenderType.entityTranslucent(texture);
+        } else return super.getRenderType(animatable, texture, bufferSource, partialTick);
     }
 }
