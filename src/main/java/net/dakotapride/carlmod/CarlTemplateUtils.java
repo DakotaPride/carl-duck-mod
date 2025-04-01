57ca68d9b6da59ad885c3a094541d4b593ca1e6c
Dakota <rjtube13@gmail.com>
2025-04-01 08:56:36 -0500

    Start porting to 1.21.4

--- src/main/java/net/dakotapride/carlmod/CarlTemplateUtils.java
+++ src/main/java/net/dakotapride/carlmod/CarlTemplateUtils.java
@@ -12,15 +12,15 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class CarlTemplateUtils {
-    private static final ChatFormatting TITLE_FORMAT = ChatFormatting.GRAY;
+    //private static final ChatFormatting TITLE_FORMAT = ChatFormatting.GRAY;
     private static final ChatFormatting DESCRIPTION_FORMAT = ChatFormatting.BLUE;
-    private static final Component UPGRADE = Component.translatable(Util.makeDescriptionId("upgrade", ResourceLocation.fromNamespaceAndPath(CarlMod.MODID, "carl_upgrade"))).withStyle(TITLE_FORMAT);
+    //private static final Component UPGRADE = Component.translatable(Util.makeDescriptionId("upgrade", ResourceLocation.fromNamespaceAndPath(CarlMod.MODID, "carl_upgrade"))).withStyle(TITLE_FORMAT);
     private static final Component UPGRADE_APPLIES_TO = Component.translatable(Util.makeDescriptionId("item", ResourceLocation.fromNamespaceAndPath(CarlMod.MODID, "smithing_template.carl_upgrade.applies_to"))).withStyle(DESCRIPTION_FORMAT);
     private static final Component UPGRADE_INGREDIENTS = Component.translatable(Util.makeDescriptionId("item", ResourceLocation.fromNamespaceAndPath(CarlMod.MODID, "smithing_template.carl_upgrade.ingredients"))).withStyle(DESCRIPTION_FORMAT);
     private static final Component UPGRADE_BASE_SLOT_DESCRIPTION = Component.translatable(Util.makeDescriptionId("item", ResourceLocation.fromNamespaceAndPath(CarlMod.MODID, "smithing_template.carl_upgrade.base_slot_description")));
     private static final Component UPGRADE_ADDITIONS_SLOT_DESCRIPTION = Component.translatable(Util.makeDescriptionId("item", ResourceLocation.fromNamespaceAndPath(CarlMod.MODID, "smithing_template.carl_upgrade.additions_slot_description")));
-    private static final ResourceLocation EMPTY_SLOT_BUCKET = ResourceLocation.fromNamespaceAndPath(CarlMod.MODID, "item/empty_slot_bucket");
-    private static final ResourceLocation EMPTY_SLOT_CARL = ResourceLocation.fromNamespaceAndPath(CarlMod.MODID, "item/empty_slot_carl");
+    private static final ResourceLocation EMPTY_SLOT_BUCKET = ResourceLocation.fromNamespaceAndPath(CarlMod.MODID, "container/slot/bucket");
+    private static final ResourceLocation EMPTY_SLOT_CARL = ResourceLocation.fromNamespaceAndPath(CarlMod.MODID, "container/slot/carl");
     private static final List<ResourceLocation> SLOT_LIST_0 = List.of(EMPTY_SLOT_BUCKET);
     private static final List<ResourceLocation> SLOT_LIST_1 = List.of(EMPTY_SLOT_CARL);
 
